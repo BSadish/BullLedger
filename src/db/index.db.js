6fb2b555784af674a671ddbf3abc964347f92004
BSadish <sadishpanta404@gmail.com>
2026-08-19 23:38:59 +0545

    manages the mongodb connection

---
+++ src/db/index.db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const connectDB=async()=>{
+ 
+     const connectionInstance=   await mongoose.connect(process.env.MONGODB_URL);
+     console.log(`Mongodb Connected: ${connectionInstance.connection.host}`);
+    };
+
+export default connectDB;
