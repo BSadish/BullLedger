f4a63de2754c559815857fa1ccec8c04a8fcdf9c
BSadish <sadishpanta404@gmail.com>
2026-08-20 22:37:55 +0545

    mounting a router

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 import express from "express"
-
+import authRouter from "./route/auth.routes.js"
+import coookieParser from "cookie-parser"
 const app=express()
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 
 
 
-app.get('/',(req,res)=>{
-    console.log("Hello World")
-})
+app.use("/api/auth",authRouter)
 
 export {app}
