8f3be68dbb940356f2efbc2cd5c5cb2b293ff819
BSadish <sadishpanta404@gmail.com>
2026-08-21 22:54:16 +0545

    cookie parser added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import express from "express"
-import authRouter from "./route/auth.routes.js"
 import coookieParser from "cookie-parser"
+
 const app=express()
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(coookieParser())
 
 
+/**
+ * - Route required
+ */
+import accountRouter from "./route/account.routes.js"
+import authRouter from "./route/auth.routes.js"
 
+
+/**
+ * - Use Routes
+ */
 app.use("/api/auth",authRouter)
+app.use ("/api/accounts",accountRouter)
 
 export {app}
