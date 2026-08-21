9111da0b7ed8f20ca34ece7eaef93ff434c05bf5
BSadish <sadishpanta404@gmail.com>
2026-08-21 22:54:42 +0545

    implemented simpler version of dotenv

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
-
+import "dotenv/config";
 import { app } from "./app.js"
-import dotenv from "dotenv"
+
 import connectDB from "./db/index.db.js"
 
 
-dotenv.config({
-    path:"./.env"
-})
 
 connectDB() //return a promise as it is an async function as succes proceed to .then else goes to catch
 .then(()=>{
