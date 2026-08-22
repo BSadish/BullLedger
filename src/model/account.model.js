15dde88df02b324f2f44a8427858de0e3cf6eb47
BSadish <sadishpanta404@gmail.com>
2026-08-22 20:15:43 +0545

    feat: add account model

--- src/model/account.model.js
+++ src/model/account.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Ledger } from "./ledger.model.js";
 
 const accountSchema=new mongoose.Schema(
     
@@ -14,8 +15,8 @@ const accountSchema=new mongoose.Schema(
 
             status:{
                 type:String,
-                enum:["Active","Frozen","Closed"],
-                default:'Active'
+                enum:["ACTIVE","FROZEN","CLOSED"],
+                default:'ACTIVE'
             },
             currency:{
                 type:String,
@@ -29,5 +30,45 @@ const accountSchema=new mongoose.Schema(
     ,{timestamps:true})
 
     accountSchema.index({user:1,status:1})
+
+    accountSchema.methods.getBalance=async function(){
+        const balanceData=await Ledger.aggregate(
+            [
+                {$match:{account:this._id}},
+                {
+                    $group:{
+                        _id:null,
+                        totalDebit:{
+                            $sum:{
+                                $cond:[
+                                   { $eq:["$type","DEBIT"]},
+                                   "$amount",0
+                                
+                                ]
+                            }
+                        },
+                        totalCredit:{
+                            $sum:{
+                                 $cond:[
+                                   { $eq:["$type","CREDIT"]},
+                                   "$amount",0
+                                
+                                ]
+                            }
+                        }
+                    }
+                },
+               { 
+                $project:{
+                    _id:0,
+                    balance:{$subtract:["$totalCredit","$totalDebit"]}
+                }}
+            ]
+        )
+        if(balanceData.length===0){
+            return 0
+        }
+        return balanceData[0].balance
+    }
 
     export const Account=mongoose.model("Account",accountSchema)
