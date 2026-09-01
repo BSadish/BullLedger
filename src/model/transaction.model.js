221ff4d63f28e0a41a6d5eee7cc497ece64abe70
BSadish <sadishpanta404@gmail.com>
2026-09-01 20:19:26 +0545

    status schema was mismatched now solved

--- src/model/transaction.model.js
+++ src/model/transaction.model.js
@@ -1,43 +1,41 @@
 import mongoose from "mongoose";
 
-const transactionSchema=new mongoose.Schema(
+const transactionSchema = new mongoose.Schema(
     {
-        fromAccount:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"account",
-            required:[true,"Transaction must be associated with the from account"],
-            index:true
+        fromAccount: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "account",
+            required: [true, "Transaction must be associated with the from account"],
+            index: true
         },
-        toAccount:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"account",
-            required:[true,"Transaction must be associated with the to account"],
-            index:true
+        toAccount: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "account",
+            required: [true, "Transaction must be associated with the to account"],
+            index: true
         },
-        status:{
-            status:{
-                type:String,
-                enum:{
-                    values:['PENDING','COMPLETED','FAILED','REVERED'],
-                    message:"Status can be either Pending,Completed, Failed or Reversed"
-                },
-                default:"PENDING"
-            }
+        status: {
+            type: String,
+            enum: {
+                values: ['PENDING', 'COMPLETED', 'FAILED', 'REVERSED'],
+                message: "Status can be either Pending, Completed, Failed or Reversed"
             },
-            amount:{
-                type:Number,
-                required:[true,"Amount is required"],
-                min:[0,"Transaction amount cannot be negative"]
-            },
-            idempotencyKey:{
-                type:String,
-                required:[true,"Idempotency key requied for creating a transaction"],
-                index:true
-            }
-        
+            default: "PENDING"
+        },
+        amount: {
+            type: Number,
+            required: [true, "Amount is required"],
+            min: [0, "Transaction amount cannot be negative"]
+        },
+        idempotencyKey: {
+            type: String,
+            required: [true, "Idempotency key requied for creating a transaction"],
+            index: true
+        }
+
     }
-    ,{timestamps:true})
+    , { timestamps: true })
 
 
 
-export const Transaction=mongoose.model("Transaction",transactionSchema)
+export const Transaction = mongoose.model("Transaction", transactionSchema)
