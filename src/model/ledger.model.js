257398b403b9b73179e4360855038583889f8c9c
BSadish <sadishpanta404@gmail.com>
2026-09-13 21:36:14 +0545

    type schema enum has issues with baracket

--- src/model/ledger.model.js
+++ src/model/ledger.model.js
@@ -1,44 +1,48 @@
 import mongoose from "mongoose";
 
-const ledgerSchema=new mongoose.Schema(
+const ledgerSchema = new mongoose.Schema(
     {
-        account:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"account",
-            required:[true, "Ledger must be associated with an account"],
-            index:true,
-            immutable:true
+        account: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "account",
+            required: [true, "Ledger must be associated with an account"],
+            index: true,
+            immutable: true
         },
-        transaction:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"transaction",
-            required:[true,"Ledger must be associated wiht a transaction"],
-            index:true,
-            immutable:true
+        transaction: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "transaction",
+            required: [true, "Ledger must be associated wiht a transaction"],
+            index: true,
+            immutable: true
         },
-        type:{
-            type:String,
-            enum:{
-                values:["CREDIT","DEBIT"],
-                message:"Type can be either CREDIT OR DEBIT"
+        amount: {
+            type: Number,
+            required: true
+        },
+        type: {
+            type: String,
+            enum: {
+                values: ["CREDIT", "DEBIT"],
+                message: "Type can be either CREDIT OR DEBIT"
             },
-            required:[true,"Ledger type is required"],
-            immutable:true
+            required: [true, "Ledger type is required"],
+            immutable: true
         }
     }
-    ,{timestamps:true})
+    , { timestamps: true })
 
-    function preventLedgerModification(){
-        throw new Error("Ledger entries are immutable and cannot be modified or deleted");
-    }
-    ledgerSchema.pre('findOneAndUpdate',preventLedgerModification)
-    ledgerSchema.pre('updateOne',preventLedgerModification)
-    ledgerSchema.pre('deleteOne',preventLedgerModification)
-    ledgerSchema.pre('remove',preventLedgerModification)
-    ledgerSchema.pre('deleteMany',preventLedgerModification)
-    ledgerSchema.pre('updateMany',preventLedgerModification)
-    ledgerSchema.pre('findOneAndDelete',preventLedgerModification)
-    ledgerSchema.pre('findOneAndReplace',preventLedgerModification)
+function preventLedgerModification() {
+    throw new Error("Ledger entries are immutable and cannot be modified or deleted");
+}
+ledgerSchema.pre('findOneAndUpdate', preventLedgerModification)
+ledgerSchema.pre('updateOne', preventLedgerModification)
+ledgerSchema.pre('deleteOne', preventLedgerModification)
+ledgerSchema.pre('remove', preventLedgerModification)
+ledgerSchema.pre('deleteMany', preventLedgerModification)
+ledgerSchema.pre('updateMany', preventLedgerModification)
+ledgerSchema.pre('findOneAndDelete', preventLedgerModification)
+ledgerSchema.pre('findOneAndReplace', preventLedgerModification)
 
 
-    export const Ledger=mongoose.model("Ledger",ledgerSchema)
+export const Ledger = mongoose.model("Ledger", ledgerSchema)
