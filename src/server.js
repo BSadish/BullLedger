593c98b1b33665439d0ab31f373bc31ba5d589c4
BSadish <sadishpanta404@gmail.com>
2026-09-15 22:08:51 +0545

    dotenv modified

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { app } from "./app.js"
 
 import connectDB from "./db/index.db.js"
 
+dotenv.config({
+    path:"./.env"
+})
 
 
 connectDB() //return a promise as it is an async function as succes proceed to .then else goes to catch
