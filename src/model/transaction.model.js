0f9b95329da70e6ebcdea510ae369b3772461183
BSadish <sadishpanta404@gmail.com>
2026-09-15 20:34:13 +0545

    from and to account model name is fixed and well situated with account model name as "Account"

--- src/model/transaction.model.js
+++ src/model/transaction.model.js
@@ -4,13 +4,13 @@ const transactionSchema = new mongoose.Schema(
     {
         fromAccount: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "account",
+            ref: "Account",
             required: [true, "Transaction must be associated with the from account"],
             index: true
         },
         toAccount: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "account",
+            ref: "Account",
             required: [true, "Transaction must be associated with the to account"],
             index: true
         },
