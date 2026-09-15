6c56c2c3cf6643e6257ede1d87d4afa3480463ce
BSadish <sadishpanta404@gmail.com>
2026-09-15 22:24:35 +0545

    deploy code sample

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(coookieParser())
 
+app.get('/',(req,res)=>{
+    console.log("My first deployment project")
+})
 
 /**
  * - Route required
